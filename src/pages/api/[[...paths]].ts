7356d39ba830e1bab48fc529ee517f9708c26fe1
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-08-04 10:21:54 -0300

    feat: config api

---
+++ src/pages/api/[[...paths]].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
+
+import routes from "@/api/routes";
+import ApiError from "@/utils/ApiError";
+
+const api = nc<NextApiRequest, NextApiResponse>({
+  attachParams: true,
+  onNoMatch: (req, res) => {
+    res.status(404).end();
+  },
+  onError: (error, req, res) => {
+    let status = 500;
+    let message = "Ocorreu um erro, tente novamente!";
+    let cause: string = "Erro indefinido";
+
+    if (error instanceof ApiError) {
+      status = error.status;
+      message = error.message;
+    }
+    cause = error.message;
+
+    res.status(status).json({ message, cause });
+  },
+});
+
+export const config = { api: { bodyParser: false } };
+
+export default api.use("/api", routes(api));
