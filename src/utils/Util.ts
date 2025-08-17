c38b9c3e71f45dcd60ffd1c38083d98a825a7506
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-08-17 18:37:52 -0300

    feat: add method checkHash

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -4,4 +4,8 @@ export default class Util {
   static async hash(value: string, salt = 8): Promise<string> {
     return bcrypt.hash(value, salt);
   }
+
+  static checkHash(value: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(value || "", hash || "");
+  }
 }
