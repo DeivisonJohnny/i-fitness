11f56e40467b7912b74ac02644ad9c0fae902970
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-08-17 18:49:37 -0300

    feat: add button logou in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,9 @@
 import { usePathname } from "next/navigation";
 import { SidebarTrigger } from "../ui/sidebar";
 import { ThemeToggle } from "../ui/them-toggle";
+import { Button } from "../ui/button";
+import { LogOut, LogOutIcon } from "lucide-react";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function Header() {
   const pathname = usePathname();
@@ -13,16 +16,27 @@ export default function Header() {
   const showSidebar =
     !initialPage && !isLoginPage && !isSignupPage && !isComplet;
 
+  const { logout } = useAuth();
+
   return (
     <>
       {showSidebar && (
-        <header className="flex h-[66px] shrink-0 items-center gap-2 border-b px-4 sticky top-0 z-10 bg-white dark:bg-background ">
+        <header className="flex h-[66px] shrink-0 items-center gap-2 bsorder-b px-4 sticky top-0 z-10 bg-white dark:bg-background ">
           <SidebarTrigger className="-ml-1 text-black dark:text-white" />
           <div className="flex flex-1 items-center justify-between">
             <h1 className="text-lg font-semibold text-black dark:text-white ">
               Dashboard
             </h1>
-            <ThemeToggle />
+            <div className="flex items-center gap-2.5">
+              <Button
+                onClick={() => logout()}
+                variant={"outline"}
+                className="h-9 w-9 bg-white dark:bg-background  text-black dark:text-white  "
+              >
+                <LogOutIcon />
+              </Button>
+              <ThemeToggle />
+            </div>
           </div>
         </header>
       )}
