7f2dc8fa1a83da7986faa61e1a8e7c1b0c92051d
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-09-04 20:57:04 -0300

    feat: update constants key_secret_jwt

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -1,2 +1,3 @@
-export const KEY_SECRET_JWT = "ashdy9asydjasdya7usjdasyduaihsd";
+export const KEY_SECRET_JWT =
+  process.env.JWT_SECRET || "eXsjvAP9gHrje4hI0QlcjqeiBYkb7GWkV8hbV+VkxMY=";
 export const TOKEN_KEY = "token_auth";
