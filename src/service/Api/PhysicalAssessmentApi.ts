4821dd45bce58c597ab04c3e4169a69bdebea918
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-09-04 20:57:43 -0300

    feat: create type for return PhysicalAssessment

--- src/service/Api/PhysicalAssessmentApi.ts
+++ src/service/Api/PhysicalAssessmentApi.ts
@@ -1,7 +1,26 @@
 import Api from ".";
+export type PhysicalAssessmentResponse = {
+  physicalAssessment: {
+    id: string;
+    userId: string;
+    bmi: number;
+    bmiClassification: string;
+    bmr: number;
+    tdee: number;
+    dailyCaloricTarget: number;
+    dailyCaloricTargetExplanation: string;
+    weightGoalRecommendation: string;
+    proteinsGrams: number;
+    carbohydratesGrams: number;
+    fatsGrams: number;
+    generalRecommendations: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+};
 
 export class PhysicalAssessmentApi {
-  static async create() {
+  static async create(): Promise<PhysicalAssessmentResponse> {
     return Api.post("/assessment");
   }
 }
