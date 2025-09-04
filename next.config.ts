b619c7f3a054dafe025b0df357e683c3f1927ca7
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-09-04 20:52:41 -0300

    feat: schema and next

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,37 @@
 import type { NextConfig } from "next";
 
 const withPWA = require("next-pwa")({
-  dest: "public", // Diretório de destino para os arquivos do service worker
-  register: true, // Registrar o service worker automaticamente
-  skipWaiting: true, // Forçar a ativação do novo service worker
+  dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.vercel-storage.com", // Permite qualquer subdomínio do vercel-storage.com
+        hostname: "*.vercel-storage.com",
         port: "",
-        pathname: "/**", // Permite qualquer caminho de arquivo
+        pathname: "/**",
       },
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
   },
 };
 
